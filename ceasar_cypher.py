d33647a93e7490582cffa471f898a40b03cbfe6a
Joseph Swanson <joe@swantron.com>
2022-03-23 20:54:38 -0600

    refactor cypher

--- ceasar_cypher.py
+++ ceasar_cypher.py
@@ -4,25 +4,15 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
+def ceasar(plain_text, shift_amount, direction_way):
   cipher_text = ""
   for letter in plain_text:
     position = alphabet.index(letter)
-    new_position = position + shift_amount
+    if direction == "encode":    
+      new_position = position + shift_amount
+    else:
+      new_position = position - shift_amount
     cipher_text += alphabet[new_position]
   print(f"The encoded text is {cipher_text}")
 
-def decrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    cipher_text += alphabet[new_position]
-  print(f"The decoded text is {cipher_text}")
-  
-if direction == "encode":
-  encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-  decrypt(plain_text=text, shift_amount=shift)
-else:
-  print("You entered an invalid option")
+ceasar(plain_text=text, shift_amount=shift, direction_way=direction)
